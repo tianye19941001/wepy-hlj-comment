a09da996445ac9bded4027e3a9db5ccff9e90893
原野 <yuanye@yuanyedeMacBook-Pro.local>
2017-12-01 16:37:13 +0800

    add

---
+++ README.md
@@ -0,0 +1,8 @@
+# 河狸家小程序评论模块
+
+## 安装
+
+~~~sh
+npm install wepy-hlj-comment --save
+~~~
+
---
+++ spec/commentList_spec.js
@@ -0,0 +1,12 @@
+import commentList from '../src/commentList';
+
+describe('test @hlj/commentList', () => {
+ 
+  it('works', () => {
+    expect(true).to.equal(true);
+  });
+
+  //TODO:
+  //补上更多测试
+ 
+});
